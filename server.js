7f0124c2fb6065d4c728fd4e5c29ad4b41c3a101
unitesgroup2022-max <unitesgroup2022@gmail.com>
2026-03-27 19:11:35 +0630

    Update server.js to change port and fix responses

--- server.js
+++ server.js
@@ -1,36 +1,33 @@
-const express = require("express");
-const { exec } = require("child_process");
+const express = require('express');
+const { exec } = require('child_process');
 
 const app = express();
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 3000;
 
-// Home
-app.get("/", (req, res) => {
-  res.send(" Server running...");
+app.get('/', (req, res) => {
+  res.send('Server running ');
 });
 
-// Download endpoint
-app.get("/download", (req, res) => {
+app.get('/download', (req, res) => {
   const url = req.query.url;
 
   if (!url) {
-    return res.send(" Please provide URL");
+    return res.send('No URL provided');
   }
 
-  const command = `yt-dlp -f best -o - "${url}"`;
+  const command = `yt-dlp -o - "${url}"`;
 
-  exec(command, { maxBuffer: 1024 * 1024 * 50 }, (error, stdout, stderr) => {
-    if (error) {
-      console.error(error);
-      return res.send(" Download failed");
+  exec(command, { maxBuffer: 1024 * 1024 * 50 }, (err, stdout, stderr) => {
+    if (err) {
+      console.log(err);
+      return res.send('Download failed');
     }
 
-    res.setHeader("Content-Disposition", "attachment; filename=video.mp4");
+    res.setHeader('Content-Disposition', 'attachment; filename=video.mp4');
     res.send(stdout);
   });
 });
 
-// Start server
 app.listen(PORT, () => {
   console.log(` Server running on port ${PORT}`);
 });

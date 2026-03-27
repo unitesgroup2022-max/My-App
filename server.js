ffb28d4cae37e3e8391e8af394b507e27c09dad6
unitesgroup2022-max <unitesgroup2022@gmail.com>
2026-03-27 14:43:56 +0630

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,3 @@
-const express = require('express');
-const ytdl = require('ytdl-core');
-
-const app = express();
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
-
 app.get('/api', async (req, res) => {
   const url = req.query.url;
 
@@ -16,11 +7,14 @@ app.get('/api', async (req, res) => {
 
   try {
     res.header('Content-Disposition', 'attachment; filename="video.mp4"');
-    ytdl(url, { format: 'mp4' }).pipe(res);
+    res.header('Content-Type', 'video/mp4');
+
+    ytdl(url, {
+      filter: 'audioandvideo',
+      quality: 'highest'
+    }).pipe(res);
+
   } catch (err) {
     res.send("Download failed");
   }
 });
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("Server running"));
